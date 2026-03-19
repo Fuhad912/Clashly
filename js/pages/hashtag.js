43d1cfe6dc20e0ee161316350812bd75daba8029
Fuhad912 <basiratbello912@gmail.com>
2026-03-19 11:56:37 +0100

    Update hashtag.js

--- js/pages/hashtag.js
+++ js/pages/hashtag.js
@@ -141,9 +141,14 @@
         throw voteResult.error;
       }
 
+      // Trust optimistic user_vote — server re-read can be stale due to replication lag
+      const reconciledVote = voteResult.vote ? {
+        ...voteResult.vote,
+        user_vote: optimisticVote ? optimisticVote.user_vote : voteResult.vote.user_vote,
+      } : optimisticVote;
       updateTakeVoteState(input.takeId, {
         vote_loading: false,
-        vote: voteResult.vote,
+        vote: reconciledVote,
       });
       syncHashtagTakeState(input.takeId);
       setFeedState("", "");
